5e6041aa48b664bf17639b8353f0c0868a1438bb
sofia <sofia@changex.org>
2020-09-16 19:53:16 +0300

    When creating a task, attach it to the user logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  def context
+    { user: logged_in_user }
+  end
+
 end
--- app/resources/task_resource.rb
+++ app/resources/task_resource.rb
@@ -1,4 +1,10 @@
 class TaskResource < JSONAPI::Resource
   attributes :title
   has_many :tags
+
+  before_create :add_user
+
+  def add_user
+    @model.user = @context[:user]
+  end
 end
