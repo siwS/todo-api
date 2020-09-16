744ed2e7f9fc441b7c84371f9b8789bf1078b596
sofia <sofia@changex.org>
2020-09-16 20:13:24 +0300

    Do not allow to modify tasks that do not belong to user.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,8 @@
 class TasksController < ApplicationController
+  before_action :ensure_belongs_to_user, only: [:update]
+
+  def ensure_belongs_to_user
+    @task = Task.find(params[:id])
+    head :forbidden unless @task.user == logged_in_user
+  end
 end
