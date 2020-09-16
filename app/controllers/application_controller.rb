abd447a7da907fe01be966b467ebd375cad819b5
sofia <sofia@changex.org>
2020-09-16 18:05:48 +0300

    Extract Authenticatable to module and fix indentation issues.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,45 +1,11 @@
 class ApplicationController < ActionController::Base
   include JSONAPI::ActsAsResourceController
+  include ::JwtAuthenticatable
+
+  before_action :authorized
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  before_action :authorized
-
-  def encode_token(payload)
-    JWT.encode(payload, 's3cr3t')
-  end
-
-  def auth_header
-    # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
-  end
-
-  def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1]
-      # header: { 'Authorization': 'Bearer <token>' }
-      begin
-        JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        nil
-      end
-    end
-  end
-
-  def logged_in_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
-  end
-
-  def logged_in?
-    !!logged_in_user
-  end
-
-  def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,24 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: {error: "Invalid username or password"}
+      render json: { error: "Invalid username or password" }
     end
   end
 
-  # LOGGING IN
+  # LOGIN
   def login
     @user = User.find_by(username: params[:username])
 
     if @user && @user.authenticate(params[:password])
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: {error: "Invalid username or password"}
+      render json: { error: "Invalid username or password" }
     end
   end
-
 
   def auto_login
     render json: @user
@@ -34,6 +33,4 @@ class UsersController < ApplicationController
   def user_params
     params.permit(:username, :password)
   end
-
-
 end
