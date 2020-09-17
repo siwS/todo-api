46a5e9995f1bf51fababc1e8f6dddcdb6f388881
sofia <sofia@changex.org>
2020-09-17 16:17:53 +0300

    Add funds for Chicago.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,31 @@
 class TasksController < ApplicationController
   before_action :ensure_belongs_to_user, only: [:update]
+  before_action :transform_tags_attribute_to_relationship, only: [:update]
 
   def ensure_belongs_to_user
     @task = Task.find(params[:id])
     head :forbidden unless @task.user == logged_in_user
+  end
+
+  # Backwards support for tags as attributes.
+  def transform_tags_attribute_to_relationship
+    tags_attribute = params["data"]["attributes"]["tags"]
+    return unless tags_attribute.present?
+
+    params["data"]["relationships"] = create_tags_relationship_hash(tags_attribute)
+    params["data"]["attributes"].delete("tags")
+  end
+
+  private
+
+  def create_tags_relationship_hash(tags_attribute)
+    tag_records = tags_attribute.map{ |tag| Tag.find_or_create_by(name: tag) }
+    json = tag_records.map { |tag| { type: "tags", id: tag.id } }
+
+    {
+      "tags": {
+        "data": json
+      }
+    }
   end
 end
